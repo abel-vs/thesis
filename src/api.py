d80106949b6d59dd6a6582a5902a5eb4852e040b
abel <a.vansteenweghen@student.tudelft.nl>
2023-02-15 19:11:48 +0000

    Add API logic for compressing models

--- src/api.py
+++ src/api.py
@@ -7,17 +7,23 @@ import uvicorn
 import analysis
 from pydantic import BaseModel
 import json
-
+import general
+import plot
+import utils
+import importlib
+import torch
+from tempfile import SpooledTemporaryFile, NamedTemporaryFile
+import mnist
+import torch.nn.functional as F
+import metrics
+import evaluation as eval
 
 HOST = "127.0.0.1"
 PORT = 8000
 
 app = FastAPI()
 
-origins = [
-    "http://" + HOST + ":" + str(PORT),
-    "http://localhost:3000"
-]
+origins = ["http://" + HOST + ":" + str(PORT), "http://localhost:3000"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -27,11 +33,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class AnalysisModel(BaseModel):
-    compression_goal: str   # Goal of the compression (model_size, inference_time, energy_usage).
-    compression_target: float       # Target value that the model should achieve after compression, as percentage of the original value.
-    performance_metric: str    # Metric used to measure the performance of the model.
-    performance_target: float       # Target value that the model should achieve after compression.
+    compression_goal: str  # Goal of the compression (model_size, inference_time, energy_usage).
+    compression_target: float  # Target value that the model should achieve after compression, as percentage of the original value.
+    performance_metric: str  # Metric used to measure the performance of the model.
+    performance_target: float  # Target value that the model should achieve after compression.
 
     @classmethod
     def __get_validators__(cls):
@@ -46,9 +53,10 @@ class AnalysisModel(BaseModel):
             print("Object:", value.keys())
             return value
 
+
 class CompressionModel(BaseModel):
-    type: str                # Type of compression
-    name: str                      # Target value that the model should achieve after compression, as percentage of the original value.
+    type: str  # Type of compression
+    name: str  # Target value that the model should achieve after compression, as percentage of the original value.
     # target: float = 69         # Target value that the model should achieve after compression.
 
     @classmethod
@@ -63,7 +71,8 @@ class CompressionModel(BaseModel):
 
 
 class CompressionList(BaseModel):
-    '''Passing list of compression actions directly to API is not possible, therefore this model'''
+    """Passing list of compression actions directly to API is not possible, therefore this model"""
+
     actions: List[CompressionModel]
 
     @classmethod
@@ -84,15 +93,20 @@ async def home():
 
 # Analze the given model and return suggested compression actions
 @app.post("/analyze")
-def analyze(settings: AnalysisModel = Form(...), model_state: UploadFile = File(...), model_architecture: UploadFile = File(...)):
+def analyze(
+    settings: AnalysisModel = Form(...),
+    model_state: UploadFile = File(...),
+    model_architecture: UploadFile = File(...),
+):
     print("Settings", settings)
     compression_actions = analysis.analyze(
-        model_state, 
-        model_architecture, 
+        model_state,
+        model_architecture,
         settings.compression_goal,
-        settings.compression_target, 
-        settings.performance_metric, 
-        settings.performance_target) 
+        settings.compression_target,
+        settings.performance_metric,
+        settings.performance_target,
+    )
 
     return {"compression_actions": compression_actions, "settings": settings}
 
@@ -100,15 +114,36 @@ def analyze(settings: AnalysisModel = Form(...), model_state: UploadFile = File(
 # Analze the given model and return suggested compression actions
 @app.post("/compress")
 def compress(
-    compression_actions: CompressionList = Form(...), 
-    model_state: UploadFile = File(...), 
-    model_architecture: UploadFile = File(...)):
+    compression_actions: CompressionList = Form(...),
+    model_state: UploadFile = File(...),
+    model_architecture: UploadFile = File(...),
+):
 
-    return {"compression_actions": compression_actions.actions}
+    model_state_file: NamedTemporaryFile = utils.spooled_to_named(
+        model_state.file, suffix=".pth"
+    )
+    model_architecture_file: NamedTemporaryFile = utils.spooled_to_named(
+        model_architecture.file, suffix=".py"
+    )
+
+    model = torch.load(model_state_file.name)
+
+    # Compress the model
+    compressed_model = general.compress_model(model, compression_actions.actions)
+
+    # Evaluate the compressed model
+    original_results = eval.get_results(model, mnist.test_loader)
+    compressed_results = eval.get_results(compressed_model, mnist.test_loader)
+
+    return {
+        "original_results": original_results,
+        "compressed_results": compressed_results,
+    }
 
 
 def main(host, port):
     uvicorn.run("api:app", host=host, port=port, reload=True)
+
 
 # Run the file to start the api server
 if __name__ == "__main__":
