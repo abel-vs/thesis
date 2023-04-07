c73cda60a4459572c7d9f6ca318a7e29f8034897
abel <a.vansteenweghen@student.tudelft.nl>
2023-04-07 19:11:05 +0100

    Refactor static quantization

--- src/compression/quantization.py
+++ src/compression/quantization.py
@@ -1,33 +1,66 @@
+import copy
 import torch
-import general
+import torch.nn as nn
+from torch.quantization import QuantStub, DeQuantStub
+
+
+class QuantizedModelWrapper(nn.Module):
+    def __init__(self, model):
+        super(QuantizedModelWrapper, self).__init__()
+        self.quant = QuantStub()
+        self.model = model
+        self.dequant = DeQuantStub()
+
+    def forward(self, x):
+        x = self.quant(x)
+        x = self.model(x)
+        return self.dequant(x)
+
 
 # Method that performs static quantization on a model
 def static_quantization(model, dataset, backend="qnnpack"):
-    model.eval()
+    # Decouple the quantized model from the original model
+    model = copy.deepcopy(model)
 
-    modules_to_fuse = find_modules_to_fuse(model)
-    if not len(modules_to_fuse) == 0:
-        model = torch.quantization.fuse_modules(model, modules_to_fuse)
+    # Wrap the model in a QuantizedModelWrapper
+    quantized_model = QuantizedModelWrapper(model)
 
-    model.qconfig = torch.ao.quantization.default_qconfig
-    print(model.qconfig)
-    # model.qconfig = torch.quantization.get_default_qconfig(backend)
-    torch.backends.quantized.engine = backend
-    quantized_model = torch.quantization.prepare(model, inplace=False)
+    # TODO: Figure out how to fuse modules
+    # modules_to_fuse = find_modules_to_fuse(model)
+    # if not len(modules_to_fuse) == 0:
+    #     model = torch.quantization.fuse_modules(model, modules_to_fuse)
+
+    # Set the backend to use for quantization
+    # 'fbgemm' for server (x86), 'qnnpack' for mobile (ARM)
+    # TODO: Create option to set backend in front-end, e.g. "what device will the model be deployed on?"
+    model.qconfig = torch.quantization.get_default_qconfig(backend)
+
+    # Prepare the model for quantization
+    torch.quantization.prepare(quantized_model, inplace=True)
 
     # Calibrate with the training set
-    general.test(model, dataset)
-    print("Static Quantization: Calibration done")
+    print("Calibrating...")
+    calibrate(quantized_model, dataset.train_loader)
+    print("Calibration complete.")
 
-    quantized_model = torch.quantization.convert(quantized_model, inplace=False)
+    # Convert the model to a quantized model
+    torch.quantization.convert(quantized_model, inplace=True)
 
     return quantized_model
+
+
+def calibrate(model, data_loader):
+    model.eval()
+    with torch.no_grad():
+        for data, _ in data_loader:
+            model(data)
 
 
 # Method that performs dynamic quantization on a model
 def dynamic_quantization(model, backend="qnnpack"):
     torch.backends.quantized.engine = backend
-    quantized_model = torch.quantization.quantize_dynamic(model, dtype=torch.qint8)
+    quantized_model = torch.quantization.quantize_dynamic(
+        model, dtype=torch.qint8)
     return quantized_model
 
 
