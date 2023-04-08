ce8d7d6e2b205f300f9d1b124575f65435a037b6
abel <a.vansteenweghen@student.tudelft.nl>
2023-04-08 16:00:32 +0100

    Clean up unstructured pruning

--- src/compression/pruning.py
+++ src/compression/pruning.py
@@ -6,30 +6,19 @@ from dataset_models import DataSet
 
 """ UNSTRUCTURED PRUNING """
 
-# Method that returns the parameters to prune
-# TODO: This should be done intelligently, returning the parameters that are the least important
-# For now, we just return the parameters that are the smallest
-def get_parameters_to_prune(model, pruning_rate):
-    # Get the parameters to prune
-    parameters_to_prune = []
-    for name, parameter in model.named_parameters():
-        if parameter.requires_grad:
-            parameters_to_prune.append((name, parameter))
-    # Sort the parameters by their absolute value
-    parameters_to_prune.sort(key=lambda x: torch.sum(torch.abs(x[1])))
-    # Return the parameters to prune
-    return parameters_to_prune[: int(len(parameters_to_prune) * pruning_rate)]
-
-
 # Method that randomly prunes a module by a given rate from 0% to 100%
+
+
 def random_pruning(model, rate):
     for module in model.children():
         prune.random_unstructured(module, name="weight", amount=rate)
         prune.remove(module, "weight")
-        
+
     return model
 
 # Method that randomly prunes a module by a given rate from 0% to 100%
+
+
 def magnitude_pruning_global_unstructured(model, rate):
     for module in model.children():
         prune.l1_unstructured(module, name="weight", amount=rate)
@@ -38,33 +27,20 @@ def magnitude_pruning_global_unstructured(model, rate):
     return model
 
 
-# TODO: Define a custom pruning method that
-# Below is an example of a custom pruning method
-class CustomMethod(prune.BasePruningMethod):
-    """Prune every other entry in a tensor"""
-
-    PRUNING_TYPE = "unstructured"
-
-    def compute_mask(self, t, default_mask):
-        mask = default_mask.clone()
-        mask.view(-1)[::2] = 0
-        return mask
-
-
-
 """ STRUCTURED PRUNING """
 
 
 # Method to get the layers that should be ignored.
-# For example the final classifying layer
 # TODO: this method should find the final layer in a general way, we can't assume the final layer is the last module, since it depends on the forward function.
 def get_ignored_layers(model):
     ignored_layers = []
 
-    for i, m in enumerate(model.modules()):
-        if i == (len(list(model.modules())) -1):
-            ignored_layers.append(m)
-    
+    modules = model.children()
+    # Add first layer, which is the input layer
+    ignored_layers.append(modules[0])
+    # Add final layer, which is the last classifier layer
+    ignored_layers.append(modules[-1])
+
     return ignored_layers
 
 
